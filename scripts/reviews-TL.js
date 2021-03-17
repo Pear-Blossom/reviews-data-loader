e64c5e98ea27fe172a441fcf768e83471f0a642b
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-17 10:42:11 -0700

    testing reviews transformation with linereader

--- scripts/reviews-TL.js
+++ scripts/reviews-TL.js
@@ -2,13 +2,20 @@ const readline = require('readline');
 const fs = require('fs');
 const path = require('path');
 const db = require('../model/index.js');
+const transform = require('../transform/index.js');
 
 const file = path.resolve('test.csv');
 const reviewsInterface = readline.createInterface({
   input: fs.createReadStream(file)
 });
 
-reviewsInterface.on('line', line => console.log(`current line ${line}`));
+reviewsInterface.on('line', line => {
+  if (line.split(',')[0] !== 'id') {
+    const currentLine = transform.reviews(line);
+
+    console.log(currentLine);
+  }
+});
 
 reviewsInterface.on('close', () => {
   reviewsInterface.close();
