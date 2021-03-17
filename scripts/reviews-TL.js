f1449547345632b12b63234ccf106bf34271a4ba
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-17 11:54:16 -0700

    added logic to create a product doc if the product_id is not found

--- scripts/reviews-TL.js
+++ scripts/reviews-TL.js
@@ -9,16 +9,27 @@ const reviewsInterface = readline.createInterface({
   input: fs.createReadStream(file)
 });
 
-reviewsInterface.on('line', line => {
-  if (line.split(',')[0] !== 'id') {
-    const currentLine = transform.reviews(line);
+// reviewsInterface.on('close', () => {
+//   reviewsInterface.close();
+//   reviewsInterface.removeAllListeners();
+//   console.log(`done`);
+// });
 
-    console.log(currentLine);
+const processLines = async () => {
+  for await (const line of reviewsInterface) {
+    if (line.split(',')[0] !== 'id') {
+      const review = transform.reviews(line);
+      const product_id = review.product_id;
+
+      let test = await db.reviews.findProduct(product_id);
+      console.log(1, test);
+      if (!test) {
+        await db.reviews.createProduct(product_id);
+        test = await db.reviews.findProduct(product_id);
+      }
+      console.log(2, test)
+    }
   }
-});
+};
 
-reviewsInterface.on('close', () => {
-  reviewsInterface.close();
-  reviewsInterface.removeAllListeners();
-  console.log(`done`);
-});
+processLines();
