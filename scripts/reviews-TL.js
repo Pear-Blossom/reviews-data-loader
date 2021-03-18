032a57795590de99aa7f0711fc71da7e30e51b40
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-17 17:46:35 -0700

    handles transformation and loading in chunks of review_ids

--- scripts/reviews-TL.js
+++ scripts/reviews-TL.js
@@ -4,30 +4,38 @@ const path = require('path');
 const db = require('../model/index.js');
 const transform = require('../transform/index.js');
 
-const file = path.resolve('test.csv');
+const file = path.resolve('data/reviews.csv');
 const reviewsInterface = readline.createInterface({
   input: fs.createReadStream(file)
 });
 
-// reviewsInterface.on('close', () => {
-//   reviewsInterface.close();
-//   reviewsInterface.removeAllListeners();
-//   console.log(`done`);
-// });
+reviewsInterface.on('close', () => {
+  console.log(`doneee`);
+});
+
+let start = Number(process.argv[2]);
+let end = Number(process.argv[3]);
 
 const processLines = async () => {
   for await (const line of reviewsInterface) {
-    if (line.split(',')[0] !== 'id') {
-      const review = transform.reviews(line);
-      const product_id = review.product_id;
+    let reviewArray = line.split(',');
+    if (reviewArray[0] !== 'id') {
+      if (reviewArray[0] >= start && reviewArray[0] < end) {
+        let review = transform.reviews(line.split(','));
+        const product_id = review.product_id;
+        const review_id = review.review_id;
+        console.log(`~~ ${review_id}`);
 
-      let test = await db.reviews.findProduct(product_id);
-      console.log(1, test);
-      if (!test) {
-        await db.reviews.createProduct(product_id);
-        test = await db.reviews.findProduct(product_id);
+        // create product document if product_id is not found
+        let productDocument = await db.reviews.findProduct(product_id);
+        if (!productDocument) productDocument = await db.reviews.createProduct(product_id);
+
+        // create review document if review_id is not found
+        reviewDocument = await db.reviews.createReview(review);
+
+        productDocument.results.push(reviewDocument._id);
+        await productDocument.save();
       }
-      console.log(2, test)
     }
   }
 };
